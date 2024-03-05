c719b1584df55470ccb7afaab0a922b11761fd6d
Abhishek Kumar Maurya <115460755+abhithemauryas@users.noreply.github.com>
2024-03-06 03:01:36 +0530

    Add files via upload

---
+++ model/Product.js
@@ -0,0 +1,18 @@
+const mongoose= require("mongoose")
+
+const productSchime=  mongoose.Schema({
+     title: String,
+     image: String,
+     price: Number,
+     rating: Number
+})
+
+
+
+
+const productModel=mongoose.model("product", productSchime)
+
+
+module.exports={
+   productModel
+}
